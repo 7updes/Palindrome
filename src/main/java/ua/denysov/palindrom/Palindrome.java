7092b8381f35b6e33157e72bbe2fc2707d8ceb57
Alex <7up-des@mail.ru>
2015-06-24 23:08:00 +0300

    comments and tests

--- src/main/java/ua/denysov/palindrom/Palindrome.java
+++ src/main/java/ua/denysov/palindrom/Palindrome.java
@@ -16,26 +16,38 @@ public class Palindrome {
 
     public static int palindromeFinder(int num) {
 
+        if(num <= 0){
+            throw new IllegalArgumentException(num+" - is not natural number");
+        }
         String str = Integer.toString(num);
+        //for going in cycle from the end
         int j = str.length() -1;
+        //from both sides cycle going into middle and in each iteration
+        // right and left element increases by factor, 1, 10, 100, 1000...
         int factor = 1;
 
         for(int i = 0; i < j; i++, j--) {
+            //digit from start
             String leftStr = Character.toString(str.charAt(i));
+            //digit from the end
             String rightStr = Character.toString(str.charAt(j));
 
+            //parse to int
             int right = Integer.parseInt(leftStr) * factor;
             int left = Integer.parseInt(rightStr) * factor;
 
+            //last element as first
             num = num + left - right ;
             factor *= 10;
+            //increases number if last element greater then first
             if(right > left) {
                 num += factor;
+                //if the left and right sides of number are met
                 if(i == j-1) {
                     num += factor/10;
                 }
             }
-
+            //change string to current
             str = Integer.toString(num);
         }
         return num;
--- src/test/java/ua/denysov/palindrom/PalindromeTest.java
+++ src/test/java/ua/denysov/palindrom/PalindromeTest.java
@@ -7,5 +7,14 @@ import static org.junit.Assert.*;
 
 public class PalindromeTest {
 
+    @Test
+    public void testPalindromeFinder(){
+        assertEquals(Palindrome.palindromeFinder(76512), 76567);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPalindromeFinder1(){
+        Palindrome.palindromeFinder(-1);
+    }
 
 }
